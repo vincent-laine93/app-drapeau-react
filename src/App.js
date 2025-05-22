501ce2db480a4c904cd08e75599d57850a2a4de7
Vincent Laine <vincentlaine.238@gmail.com>
2025-05-22 16:38:41 +0200

    Ajout des routes au differente page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 const App = () => {
   return (
-      <div>
-        <h1>Hello React</h1>
-      </div>
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<Home/>} />
+              <Route path="/about" element={<About/>} />
+          </Routes>
+      </BrowserRouter>
   );
 };
 

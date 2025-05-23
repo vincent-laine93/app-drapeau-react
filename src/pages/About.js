f643235906c4aa4888909bdcb8a649dac7f242f5
Vincent Laine <vincentlaine.238@gmail.com>
2025-05-23 20:08:37 +0200

    modification de la page a propos

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,15 +4,28 @@ import Logo from "../composant/Logo";
 
 const About = () => {
     return (
-        <div>
+        <div className="about">
             <Logo/>
             <Navigation/>
-            <h1>A propos</h1>
-            <br/>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid atque deserunt enim esse eum labore maiores maxime, minus natus, necessitatibus nesciunt non obcaecati officia porro quae quibusdam reprehenderit sequi soluta tempora ullam unde ut voluptate! Accusamus aspernatur at autem ducimus fugit impedit nostrum pariatur provident quae! Aliquam amet consectetur, cum delectus deserunt distinctio ducimus eos ipsa, neque officia, quis quos reiciendis velit. Alias aliquam aliquid, aperiam aspernatur beatae culpa cupiditate dicta, earum excepturi ipsam iste molestiae molestias nam nihil nisi odit officia officiis pariatur, possimus quas quia quis quod reprehenderit sequi similique sit unde ut vel vitae voluptates. Ab architecto commodi eveniet inventore minus mollitia natus repudiandae tempore ullam voluptates. Accusantium aperiam beatae consectetur cum deserunt dignissimos dolor dolores dolorum ea error esse eveniet expedita facilis harum inventore ipsam iure minima molestiae natus nisi nobis nostrum odio, officia omnis perferendis quisquam quod ratione reprehenderit soluta sunt, vero voluptas voluptates voluptatibus. Aspernatur consequuntur distinctio eius laudantium molestias, nam provident qui quibusdam recusandae ut? Cum cumque distinctio dolor doloribus eos, esse et fuga hic id itaque laborum modi nam nemo nesciunt nihil non odit pariatur possimus ratione repudiandae similique temporibus ullam vero. Aliquam consequuntur dolorem impedit maiores nulla perferendis possimus quod vitae.</p>
-            <br/>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad adipisci aliquam dolore eos inventore numquam odio pariatur. Ab adipisci aliquam assumenda deleniti exercitationem, facilis fugiat hic, labore maiores minus mollitia, odio officia perspiciatis placeat quibusdam quod quos recusandae tenetur veniam voluptatum! Accusamus aut commodi deleniti deserunt dolore doloribus dolorum eveniet excepturi, fuga harum in ipsam iste iusto maxime nemo optio perferendis quis ratione rerum tempore velit voluptas voluptate, voluptates. Ab amet assumenda atque consequatur corporis dolor dolores error fugit incidunt laborum laudantium mollitia nam repellendus, sed ullam ut voluptatem? Ab dicta fugit illum iste itaque laudantium reprehenderit sapiente suscipit.</p>
-
+            <h1>À propos de cette application</h1>
+            <p>
+                Cette application React a été réalisée dans le cadre d’un apprentissage
+                des bases du développement avec React JS. Elle permet d’afficher et de
+                filtrer dynamiquement les pays du monde grâce à une API publique.
+            </p>
+            <p>
+                Le projet met en œuvre des fonctionnalités essentielles comme :
+                <ul>
+                    <li>la récupération de données avec <code>fetch</code>,</li>
+                    <li>la gestion de l’état avec les hooks React (<code>useState</code>, <code>useEffect</code>),</li>
+                    <li>la composition de l’interface à l’aide de composants réutilisables,</li>
+                    <li>et un design simple et responsive via SCSS.</li>
+                </ul>
+            </p>
+            <p>
+                C’est un excellent exercice pour se familiariser avec les concepts clés
+                de React tout en réalisant une interface interactive et concrète.
+            </p>
         </div>
     );
 };
